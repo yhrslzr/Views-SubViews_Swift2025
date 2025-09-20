0dbeeddfa67f3b30705395c2ac0d0c4e8acf51b1
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-19 18:45:46 -0600

    Se agregan parámetros a la subvista TrainCardView

--- Oreo/ContentView.swift
+++ Oreo/ContentView.swift
@@ -10,17 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ScrollView(){
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-            TrainCardView()
-
+            TrainCardView(name: "Tren Bala Kodama", iconColor: Color.yellow)
+            TrainCardView(name: "Tren Bala Nozomi", iconColor: Color.green)
+            TrainCardView(name: "Tren Bala Sakura", iconColor: Color.blue)
+            TrainCardView(name: "Tren Bala Hikari", iconColor: Color.orange)
+            TrainCardView(name: "Tren Bala Hayabusa", iconColor: Color.black)
+            TrainCardView(name: "JR Yamanote", iconColor: Color.teal)
+            TrainCardView(name: "JR Chuou", iconColor: Color.pink)
+            TrainCardView(name: "Sanyou Line", iconColor: Color.indigo)
+            TrainCardView(name: "Super Line Takamatsu", iconColor: Color.purple)
+            TrainCardView(name: "FujiSubaru Line", iconColor: Color.mint)
         }
     }
 }
--- Oreo/TrainCardView.swift
+++ Oreo/TrainCardView.swift
@@ -8,69 +8,46 @@
 import SwiftUI
 
 struct TrainCardView: View {
+    var name: String
+    var iconColor: Color
+    
     var body: some View {
         VStack(){
-
-        HStack(){
-
-        Spacer()
-
-        Text("Oferta")
-
-        .padding(.vertical, 4)
-
-        .padding(.horizontal, 12)
-
-        .background(Color.green)
-
-        .foregroundColor(.white)
-
-        .cornerRadius(2)
-
-        .frame( maxWidth: .infinity, alignment: .trailing)
-
+            HStack(){
+                Spacer()
+                Text("Oferta")
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 12)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(2)
+                    .frame( maxWidth: .infinity, alignment: .trailing)
+            }
+            
+            Image(systemName: "tram.circle.fill")
+                .font(.system(size: 90, weight: .light, design: .default))
+                .foregroundColor(iconColor)
+                .aspectRatio(contentMode: .fit )
+                .frame(width: 120, height: 100)
+            
+            Text(name)
+                .font(.largeTitle)
+                .lineLimit(1)
+                .truncationMode(.middle)
+                .padding(.bottom)
+                .frame( maxWidth: .infinity)
         }
-
-        Image(systemName: "tram.circle.fill")
-
-        .font(.system(size: 90, weight: .light, design: .default))
-
-        .foregroundColor(.green)
-
-        .aspectRatio(contentMode: .fit )
-
-        .frame(width: 120, height: 100)
-
-        Text("Tren Bala Hikari")
-
-        .font(.largeTitle)
-
-        .lineLimit(1)
-
-        .truncationMode(.middle)
-
-        .padding(.bottom)
-
-        .frame( maxWidth: .infinity)
-
-        }.padding()
-
+        .padding()
         .frame(maxWidth: .infinity, alignment: .leading)
-
         .background(Color.white)
-
         .cornerRadius(12)
-
         .overlay(
-
-        RoundedRectangle(cornerRadius: 12)
-
-        .stroke(Color(red: 130/255, green: 130/255, blue: 130/255, opacity: 0.2), lineWidth: 2)
-
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color(red: 130/255, green: 130/255, blue: 130/255, opacity: 0.2), lineWidth: 2)
         ).padding()
     }
 }
 
 #Preview {
-    TrainCardView()
+    TrainCardView(name: "Tren de Prueba", iconColor: Color.red)
 }
