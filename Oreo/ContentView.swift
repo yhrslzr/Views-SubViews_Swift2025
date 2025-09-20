792251a6ac65fa5d98310346ea686916d2e8aa6e
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-19 18:30:18 -0600

    Se convierte la card en una subview sin parámetros.

--- Oreo/ContentView.swift
+++ Oreo/ContentView.swift
@@ -9,65 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(){
-
-        HStack(){
-
-        Spacer()
-
-        Text("Oferta")
-
-        .padding(.vertical, 4)
-
-        .padding(.horizontal, 12)
-
-        .background(Color.green)
-
-        .foregroundColor(.white)
-
-        .cornerRadius(2)
-
-        .frame( maxWidth: .infinity, alignment: .trailing)
+        ScrollView(){
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
+            TrainCardView()
 
         }
-
-        Image(systemName: "tram.circle.fill")
-
-        .font(.system(size: 90, weight: .light, design: .default))
-
-        .foregroundColor(.green)
-
-        .aspectRatio(contentMode: .fit )
-
-        .frame(width: 120, height: 100)
-
-        Text("Tren Bala Hikari")
-
-        .font(.largeTitle)
-
-        .lineLimit(1)
-
-        .truncationMode(.middle)
-
-        .padding(.bottom)
-
-        .frame( maxWidth: .infinity)
-
-        }.padding()
-
-        .frame(maxWidth: .infinity, alignment: .leading)
-
-        .background(Color.white)
-
-        .cornerRadius(12)
-
-        .overlay(
-
-        RoundedRectangle(cornerRadius: 12)
-
-        .stroke(Color(red: 130/255, green: 130/255, blue: 130/255, opacity: 0.2), lineWidth: 2)
-
-        ).padding()
     }
 }
 
