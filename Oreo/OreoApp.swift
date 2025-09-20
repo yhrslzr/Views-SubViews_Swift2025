f4286f903778849dbd4fe6136621a8159c25aed4
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-19 18:01:34 -0600

    Initial Commit

---
+++ Oreo/OreoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OreoApp.swift
+//  Oreo
+//
+//  Created by WIN603 on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct OreoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
